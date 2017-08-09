f9e5e755bc6c369c22a7fc86155ec9d52a459ed1
Danverde <gre12014@byui.edu>
2017-08-09 12:48:06 -0600

    widget now pulls current course title via valence

--- csvChanger.js
+++ csvChanger.js
@@ -7,37 +7,75 @@ var uniq = require('array-uniq'),
     chalk = require('chalk'),
     d3 = require('d3-dsv');
 
+/*************************************************
+ * checks to see if the file name already exists
+ *************************************************/
+function fileExists(fileName, i, cb) {
+    i++;
+    var exists = false;
+    var files = fs.readdirSync('.')
+    files.forEach(function (file) {
+        if (file === fileName)
+            exists = true;
+    });
 
-// FORMAT PHONE NUMBER!
-// SORT STUDENTS ALPHABETICALLY
+    if (exists === true) {
+        var location = string.search(/\./);
+        if (location != -1) {
+            fileName.
+        }
+        fileName += "(" + i + ")";
+        fileExists(fileName, i, cb);
+    } else {
+        cb(fileName);
+    }
 
+}
 
+/********************************************
+ * writes whatever it's given to a json file
+ ********************************************/
 function writeResults(courses) {
     courses = JSON.stringify(courses, null, 3);
 
-    fs.writeFile('filteredByCourse.json', courses, function (err) {
-        if (err) console.error(chalk.red(err));
-        else console.log(chalk.green("file successfully written"));
-    });
+    var fileName = "filteredByCourse.json";
+
+    var write = function (fileName) {
+        fileName = 'jsonFiles/' + fileName;
+        fs.writeFile(fileName, courses, function (err) {
+            if (err) console.error(chalk.red(err));
+            else console.log(chalk.green("file successfully written"));
+        });
+    }
+
+    fileExists(fileName, 0, write);
+
 }
 
+/******************************************
+ * Puts the students in alphabetical order
+ ******************************************/
 function formatStudents(courses) {
     courses.forEach(function (course) {
+        //sort students
         course.students.sort(function (a, b) {
             if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
             if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
         });
-
     });
+    //console.log(JSON.stringify(courses, null, 3));
 
     writeResults(courses);
 }
 
+/************************************
+ * Organizes student data by course
+ ***********************************/
 function getCourseList(students) {
+    // get an array of each course
     var courses = students.map(function (student) {
         return student.D2L_COURSE_TITLE;
     });
-
     courses = uniq(courses);
 
     // Loop magic
@@ -51,27 +89,24 @@ function getCourseList(students) {
                 if (student.D2L_COURSE_TITLE == course) {
                     tempObj.students.push({
                         name: student.LAST_NAME + ', ' + student.FIRST_NAME,
-                        phone: '(' + student.PHONE.slice(0, 3) + ') ' + student.PHONE.slice(3, 6) + '-' + student.PHONE.slice(6, 10)
+                        phone: student.PHONE
                     });
                 }
             });
             return tempObj;
         });
 
-    console.log(finalList.length);
-    //console.log(JSON.stringify(finalList, null, 3));
-
     formatStudents(finalList);
-
-    //writeResults(finalList);
 }
 
+/****************************************
+ * reads student data from the csv
+ ***************************************/
 function getStudents() {
-    fs.readFile('StudentTestList.csv', function (err, data) {
+    fs.readFile('StudentSectionEnrollmentPhoneList.csv', 'binary', function (err, data) {
         if (err) console.error(chalk.red(err));
         data = data.toString();
         var students = d3.csvParse(data);
-        //console.log(students[0])
 
         getCourseList(students);
     })
