46cc6f8bf9a7e760f32b7f4e25a32d2bfd36c7bf
Danverde <gre12014@byui.edu>
2017-08-11 09:44:16 -0600

    now creates a list of instructors containing course and student data

--- csvChanger.js
+++ csvChanger.js
@@ -7,52 +7,102 @@ var uniq = require('array-uniq'),
     chalk = require('chalk'),
     d3 = require('d3-dsv');
 
-/*************************************************
- * checks to see if the file name already exists
- *************************************************/
-function fileExists(fileName, i, cb) {
-    i++;
-    var exists = false;
-    var files = fs.readdirSync('.')
-    files.forEach(function (file) {
-        if (file === fileName)
-            exists = true;
+
+/*************************************************************
+ * writes whatever it's given to a json file
+ * it's possible that files could be overwritten this way...
+ *************************************************************/
+function writeInstructor(instructor) {
+    var fileName = instructor.name + ".json";
+
+    instructor = JSON.stringify(instructor, null, 3);
+
+    fileName = 'jsonFiles/' + fileName;
+    fs.writeFile(fileName, instructor, function (err) {
+        if (err) console.error(chalk.red(err));
+        else console.log(chalk.green(fileName + " written"));
+    });
+    //fileExists(fileName, 0, write);
+}
+
+/***********************************************************
+ * filteres out instructors without corresponding courses
+ **********************************************************/
+function clearEmptyProfessors(instructors) {
+    //console.log(instructors.length);
+
+    var filteredInstructors = instructors.filter(function (instructor) {
+        if (instructor.courses.length >= 1)
+            return instructor;
+    });
+    //console.log(JSON.stringify(filteredInstructors, null, 3));
+    //console.log(filteredInstructors.length);
+
+    filteredInstructors.forEach(writeInstructor);
+}
+
+/*****************************************************
+ * creates a list of instructors including each
+ * course they teach and each student in that course
+ *****************************************************/
+function formatInstructors(instructors, courses) {
+    var usernames = instructors.map(function (instructor) {
+        return instructor.netId;
     });
 
-    if (exists === true) {
-        var location = string.search(/\./);
-        if (location != -1) {
-            fileName.
-        }
-        fileName += "(" + i + ")";
-        fileExists(fileName, i, cb);
-    } else {
-        cb(fileName);
-    }
+    usernames = uniq(usernames);
 
-}
-
-/********************************************
- * writes whatever it's given to a json file
- ********************************************/
-function writeResults(courses) {
-    courses = JSON.stringify(courses, null, 3);
-
-    var fileName = "filteredByCourse.json";
-
-    var write = function (fileName) {
-        fileName = 'jsonFiles/' + fileName;
-        fs.writeFile(fileName, courses, function (err) {
-            if (err) console.error(chalk.red(err));
-            else console.log(chalk.green("file successfully written"));
+    var tempObj,
+        tempCourseName,
+        finalList = usernames.map(function (username) {
+            tempObj = {
+                name: '',
+                username: username,
+                courses: []
+            };
+            instructors.forEach(function (instructor) {
+                if (instructor.netId === username) {
+                    tempObj.name = instructor.instructor.replace(/,\s/, ""); // so I don't have to save the json files by their usernames
+                    courses.forEach(function (course) {
+                        tempCourseName = course.courseName.slice(0, course.courseName.search(/\(/) - 1);
+                        //FDREL classes have PATH as their course code. Replacing PATH with the appropriate code based on section number
+                        if (instructor.course.search(/FDREL PATH/) > -1) {
+                            if (instructor.section >= 1001 && instructor.section <= 1066) {
+                                instructor.course = instructor.course.replace(/PATH/, "200");
+                            } else if ((instructor.section >= 1095 && instructor.section <= 1105) || instructor.section == 1112) {
+                                instructor.course = instructor.course.replace(/PATH/, "122");
+                            } else if (instructor.section >= 1106 && instructor.section <= 1111) {
+                                instructor.course = instructor.course.replace(/PATH/, "121");
+                            }
+                        }
+                        if (tempCourseName == instructor.course + ": " + instructor.section) {
+                            tempObj.courses.push(course);
+                        }
+                    });
+                }
+            });
+            return tempObj;
         });
-    }
 
-    fileExists(fileName, 0, write);
-
+    clearEmptyProfessors(finalList);
+    //finalList.forEach(writeInstructor);
 }
 
-/******************************************
+/*******************************************************
+ * reads in the file containing instructor information
+ *******************************************************/
+function readInstructorFile(courses) {
+    fs.readFile('OSM Instructors.csv', function (err, data) {
+        if (err) console.error(chalk.red(err));
+        data = data.toString();
+        var instructors = d3.csvParse(data),
+            tempObj;
+
+        formatInstructors(instructors, courses);
+    });
+}
+
+/*******************************************
  * Puts the students in alphabetical order
  ******************************************/
 function formatStudents(courses) {
@@ -65,7 +115,8 @@ function formatStudents(courses) {
     });
     //console.log(JSON.stringify(courses, null, 3));
 
-    writeResults(courses);
+    //writeResults(courses);
+    readInstructorFile(courses);
 }
 
 /************************************
